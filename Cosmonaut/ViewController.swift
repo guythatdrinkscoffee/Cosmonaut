e57bf7af8094aff8040b61bfb6f5078b458362ae
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-08-17 10:29:12 -0700

    Initial Commit

---
+++ Cosmonaut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cosmonaut
+//
+//  Created by J Manuel Zaragoza on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
